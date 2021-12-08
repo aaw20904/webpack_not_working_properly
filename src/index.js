07a224469e8eebfc6685c5aca707af7c37619b32
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2021-12-08 11:29:36 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './index.css';
+/*import responsiveImage1 from './img/rsp_001.jpg?sizes[]=300,sizes[]=600,sizes[]=1024,sizes[]=1280';*/
+import 'bootstrap/dist/css/bootstrap.min.css';
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.min.js');
